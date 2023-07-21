be356169d66b0c977bc2b185da0c54d1618ebf39
Oki Kehinde <olamiposi.oki@gmail.com>
2023-07-21 15:18:58 +0100

    add input validations

--- backend/contracts/GameNFT.sol
+++ backend/contracts/GameNFT.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-
 contract GamingNFT is ERC721, Ownable {
     struct GamingItem {
         uint256 id;
@@ -17,19 +15,29 @@ contract GamingNFT is ERC721, Ownable {
         string rarity;
         address owner;
         string level;
-        Ability[] abilities;
+        string abilities;
         ItemStats stats;
     }
 
-    struct Ability {
-        string name;
-        string description;
-    }
+  
 
     struct ItemStats {
         uint256 baseAttackPower;
         uint256 baseDefensePower;
         uint256 health;
+    }
+    struct GamingNFTInfo {
+        string name;
+        string description;
+        uint256 price;
+        string image;
+        string rarity;
+        string level;
+        string abilities;
+        uint256 baseAttackPower;
+        uint256 baseDefensePower;
+        uint256 health;
+        address owner;
     }
 
     constructor() ERC721("GamingNFT", "GAME") {}
@@ -37,84 +45,100 @@ contract GamingNFT is ERC721, Ownable {
     mapping(uint256 => GamingItem) private _gamingNFTs;
     uint256 private _nextNFTId = 1;
     event TokenTransfer(
-        address indexed from,               
+        address indexed from,
         address indexed to,
         uint256 tokenId
     );
     event TokenMinted(address indexed owner, uint256 tokenId);
-    event TokenUpdated(uint256 tokenId, string name, string description, uint256 price, string image, string rarity, string level, Ability[] abilities, uint256 baseAttackPower, uint256 baseDefensePower, uint256 health);
-    function mintGamingNFT(
-        string memory name,
-        string memory description,
+    event TokenUpdated(
+        uint256 tokenId,
+        string name,
+        string description,
         uint256 price,
-        string memory image,
-        string memory rarity,
-        string memory level,
-        Ability[] memory abilities,
-        uint256 baseAttackPower,
-        uint256 baseDefensePower,
-        uint256 health,
-        address owner
-    ) public onlyOwner {
-         uint256 tokenId = _nextNFTId;
+    );
+
+    function mintGamingNFT(GamingNFTInfo memory nftInfo) public onlyOwner {
+        uint256 tokenId = _nextNFTId;
         _nextNFTId++;
-        require(!_exists(tokenId) , "NFT already exists");          
-        _safeMint(owner, tokenId);
- 
-        uint256 totalAttackPower = baseAttackPower + (abilities.length * 10); // Increase attack power based on the number of abilities
-        uint256 totalDefensePower = baseDefensePower + (abilities.length * 5); // Increase defense power based on the number of abilities
 
-        ItemStats memory stats = ItemStats(totalAttackPower, totalDefensePower, health);
+        // Input validation checks
+        require(bytes(nftInfo.name).length > 0, "Invalid name");
+        require(bytes(nftInfo.description).length > 0, "Invalid description");
+        require(nftInfo.price > 0, "Invalid price");
+        require(bytes(nftInfo.image).length > 0, "Invalid image");
+        require(bytes(nftInfo.rarity).length > 0, "Invalid rarity");
+        require(bytes(nftInfo.level).length > 0, "Invalid level");
+        require(nftInfo.abilities.length > 0, "Invalid abilities");
+        require(nftInfo.baseAttackPower > 0, "Invalid baseAttackPower");
+        require(nftInfo.baseDefensePower > 0, "Invalid baseDefensePower");
+        require(nftInfo.health > 0, "Invalid health");
 
-        GamingItem memory newGamingNFT = GamingItem(
-            tokenId,
-            name,
-            description,
-            price,
-            image,
-            rarity,
-            owner
-            level,
-            abilities,
-            stats
+        require(!_exists(tokenId), "NFT already exists");
+
+        _safeMint(nftInfo.owner, tokenId);
+
+        uint256 abilitiesLength = nftInfo.abilities.length;
+        uint256 totalAttackPower = nftInfo.baseAttackPower +
+            (abilitiesLength * 10);
+        uint256 totalDefensePower = nftInfo.baseDefensePower +
+            (abilitiesLength * 5);
+
+        ItemStats memory stats = ItemStats(
+            totalAttackPower,
+            totalDefensePower,
+            nftInfo.health
         );
-        _gamingNFTs[tokenId] = newGamingNFT;
 
-       emit TokenMinted(owner, tokenId);
+        GamingItem storage newGamingNFT = _gamingNFTs[tokenId];
+        newGamingNFT.id = tokenId;
+        newGamingNFT.name = nftInfo.name;
+        newGamingNFT.description = nftInfo.description;
+        newGamingNFT.price = nftInfo.price;
+        newGamingNFT.image = nftInfo.image;
+        newGamingNFT.rarity = nftInfo.rarity;
+        newGamingNFT.owner = nftInfo.owner;
+        newGamingNFT.level = nftInfo.level;
+        newGamingNFT.stats = stats;
+        newGamingNFT.abilities = nftInfo.abilities;
+
+        emit TokenMinted(nftInfo.owner, tokenId);
     }
 
-    function transferGamingNFT(uint256 tokenId, address newOwner) public {
+    function transferGamingNFT(
+        uint256 tokenId,
+        address newOwner
+    ) public onlyOwner {
         address tokenOwner = ownerOf(tokenId);
-        require(tokenOwner == msg.sender || isApprovedForAll(tokenOwner, msg.sender), "GamingNFT: transfer caller is not owner nor approved");
-     require(_exists(tokenId), "GamingNFT: Token does not exist");
-        
+        require(
+            tokenOwner == msg.sender ||
+                isApprovedForAll(tokenOwner, msg.sender),
+            "GamingNFT: transfer caller is not owner nor approved"
+        );
+        require(_exists(tokenId), "GamingNFT: Token does not exist");
+
         _transfer(tokenOwner, newOwner, tokenId);
 
         GamingItem storage gamingNFT = _gamingNFTs[tokenId];
         gamingNFT.owner = newOwner;
 
-
         emit TokenTransfer(tokenOwner, newOwner, tokenId);
-
     }
 
     function ownerOf(uint256 tokenId) public view override returns (address) {
         return _gamingNFTs[tokenId].owner;
     }
 
-    function getGamingNFT(uint256 tokenId) public view returns (GamingItem memory) {
+    function getGamingNFT(
+        uint256 tokenId
+    ) public view returns (GamingItem memory) {
         return _gamingNFTs[tokenId];
     }
 
     function updateGamingNFT(
-        uint256 tokenId,
-        string memory name,
-        string memory description,
-        uint256 price,
         string memory image,
         string memory rarity,
         string memory level,
-        Ability[] memory abilities,
+        string memory abilities,
         uint256 baseAttackPower,
         uint256 baseDefensePower,
         uint256 health
@@ -122,29 +146,40 @@ contract GamingNFT is ERC721, Ownable {
         require(_exists(tokenId), "GamingNFT: NFT does not exist");
         GamingItem storage gamingNFT = _gamingNFTs[tokenId];
 
-        gamingNFT.name = name;
-        gamingNFT.description = description;
-        gamingNFT.price = price;
         gamingNFT.image = image;
         gamingNFT.rarity = rarity;
         gamingNFT.level = level;
         gamingNFT.abilities = abilities;
         gamingNFT.stats = ItemStats(baseAttackPower, baseDefensePower, health);
-        emit TokenUpdated(tokenId, name, description, price, image, rarity, level, abilities, baseAttackPower, baseDefensePower, health);
+        emit TokenUpdated(
+            tokenId,
+            name,
+            description,
+            price,
+        
+        );
     }
 
-    function approve(address to, uint256 tokenId) public {
-    address tokenOwner = ownerOf(tokenId);
-    require(tokenOwner == msg.sender || isApprovedForAll(tokenOwner, msg.sender), "GamingNFT: Not authorized to approve");
+    function approve(address to, uint256 tokenId) public override {
+        address tokenOwner = ownerOf(tokenId);
+        require(
+            tokenOwner == msg.sender ||
+                isApprovedForAll(tokenOwner, msg.sender),
+            "GamingNFT: Not authorized to approve"
+        );
 
-    _approve(to, tokenId);
-}
-function setApprovalForAll(address operator, bool approved) public {
-    require(operator != msg.sender, "GamingNFT: You cannot set approval for yourself");
+        _approve(to, tokenId);
+    }
 
-    _setApprovalForAll(msg.sender, operator, approved);
-}
+    function setApprovalForAll(
+        address operator,
+        bool approved
+    ) public override {
+        require(
+            operator != msg.sender,
+            "GamingNFT: You cannot set approval for yourself"
+        );
 
-
-
+        _setApprovalForAll(msg.sender, operator, approved);
+    }
 }
